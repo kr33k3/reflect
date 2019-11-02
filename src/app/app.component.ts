58756dbc24fe45baffcc571c5ba4fe0257a46c13
Benjamin Watkins <mrbenwatkins@outlook.com>
2019-11-01 20:37:41 -0400

    inititalize project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
+import { Plugins } from '@capacitor/core';
+const { SplashScreen, StatusBar } = Plugins;
+
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @Component({
   selector: 'app-root',
@@ -10,18 +11,16 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
+  constructor() {
     this.initializeApp();
   }
 
   initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
-    });
+    SplashScreen.hide().catch((err) => {
+      console.warn(err);
+      });
+      StatusBar.hide().catch((err) => {
+      console.warn(err);
+      });
   }
 }
